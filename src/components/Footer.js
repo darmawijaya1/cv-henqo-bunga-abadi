8350f0460a7401262cc73924ee2a5e79c32a6a0a
darmawijaya1 <adarmawijaya9@gmail.com>
2025-07-22 14:44:36 +0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
 export default function Footer() {
   return (
-    <footer className="bg-orange-600 text-white text-center py-5 mt-10 rounded-t-2xl shadow-elegant">
+    <footer className="bg-black-600 text-white text-center py-5 mt-10 rounded-t-2xl shadow-elegant">
       <div>
         <p className="font-bold text-lg">CV. Henqo Bunga Abadi</p>
         <p>Jl. Raden Fattah No.21, Tanjung Karang Pusat, Bandar Lampung</p>
-        <p>Email: <a href="mailto:henqobungaabadi@gmail.com">henqobungaabadi@gmail.com</a> | Telp: 082175111972</p>
+        <p>Email: <a href="mailto:henqobungaabadi@gmail.com" className="underline">henqobungaabadi@gmail.com</a> | Telp: 082175111972</p>
         <p className="text-xs mt-2">&copy; {new Date().getFullYear()} CV. Henqo Bunga Abadi. All Rights Reserved.</p>
       </div>
     </footer>
