72f63117019a767afb769980d8c5335d1e7a3cfa
darmawijaya1 <adarmawijaya9@gmail.com>
2025-07-22 12:29:28 +0700

    Update PDFViewer.js

--- src/components/PDFViewer.js
+++ src/components/PDFViewer.js
@@ -4,16 +4,22 @@ export default function PDFViewer({ file }) {
   if (!file) return null
 
   return (
-    <div className="w-full flex justify-center">
+    <div
+      className="w-full flex flex-col items-center"
+      onContextMenu={e => e.preventDefault()}
+      onDragStart={e => e.preventDefault()}
+      style={{ userSelect: "none" }}
+    >
       <iframe
-        src={file}
+        src={`${file}#toolbar=0&navpanes=0&scrollbar=0`}
         width="100%"
         height="600px"
         className="rounded-xl border shadow"
         title="PDF File"
-        style={{ minHeight: 400, maxWidth: 700 }}
+        style={{ minHeight: 400, maxWidth: 700, pointerEvents: 'auto' }}
         allowFullScreen
       />
+      {/* Tidak ada tombol download */}
     </div>
   )
 }
