42750c8d6057a8904c71abbaf564126e6872f4f1
darmawijaya1 <adarmawijaya9@gmail.com>
2025-07-23 09:37:05 +0700

    up

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://cv-henqo-bunga-abadi.vercel.app',
+  generateRobotsTxt: true, // generate robots.txt file
+  // ...more options
+}
