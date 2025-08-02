69913d8d4fb62d95b80512d8ab629ccba0310ed2
darmawijaya1 <116532006+darmawijaya1@users.noreply.github.com>
2025-08-02 14:40:48 +0700

    up

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,9 @@
-// next-sitemap.config.js
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://cv-henqo-bunga-abadi.vercel.app', // Ganti ke domain produksi Anda
-  generateRobotsTxt: true, // Auto-generate robots.txt
+  siteUrl: 'https://cv-henqo-bunga-abadi.vercel.app',
+  generateRobotsTxt: true,
+  outDir: './public',
   changefreq: 'monthly',
   priority: 0.7,
   sitemapSize: 5000,
-}
+};
